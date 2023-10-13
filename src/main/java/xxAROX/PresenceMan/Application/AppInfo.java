de35c69d727ca2af909a6035c052bfc831737cf3
xxAROX <arox.email.0@gmail.com>
2023-10-13 13:57:13 +0200

    Fixed the auto-updater

--- src/main/java/xxAROX/PresenceMan/Application/AppInfo.java
+++ src/main/java/xxAROX/PresenceMan/Application/AppInfo.java
@@ -3,10 +3,11 @@ package xxAROX.PresenceMan.Application;
 import java.util.StringJoiner;
 
 public final class AppInfo {
+    public final static int[] version = new int[]{ 1,1,2 };
     public final static String name = "Presence-Man";
-    public final static int[] version = new int[]{ 1,1,1 };
     public static String icon = "icon.png";
     public static long discord_application_id = 1133823892486623344L;
+    public final static boolean development = false;
 
     public static String getVersion(){
         return getVersion(".");
--- src/main/java/xxAROX/PresenceMan/Application/task/UpdateCheckTask.java
+++ src/main/java/xxAROX/PresenceMan/Application/task/UpdateCheckTask.java
@@ -1,9 +1,5 @@
 package xxAROX.PresenceMan.Application.task;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.vdurmont.semver4j.Semver;
 import xxAROX.PresenceMan.Application.App;
 import xxAROX.PresenceMan.Application.AppInfo;
@@ -20,7 +16,7 @@ public class UpdateCheckTask implements Runnable {
     @Override
     public void run() {
         try {
-            URL url = new URL("https://api.github.com/repos/Presence-Man/Application/releases/latest");
+            URL url = new URL("https://raw.githubusercontent.com/Presence-Man/releases/main/" + (AppInfo.development ? "dev-" : "") + "version-app.txt");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("User-Agent", AppInfo.name + "/" + AppInfo.getVersion());
@@ -33,76 +29,57 @@ public class UpdateCheckTask implements Runnable {
             StringBuilder builder = new StringBuilder();
             while ((read = in.read(bytes)) != -1) builder.append(new String(bytes, 0, read));
             con.disconnect();
+            String latestVersion = builder.toString().trim();
 
-            JsonObject object = JsonParser.parseString(builder.toString()).getAsJsonObject();
-            String latestVersion = object.get("tag_name").getAsString().substring(1);
             boolean updateAvailable;
             try {
                 Semver versionSemver = new Semver(AppInfo.getVersion());
                 Semver latestVersionSemver = new Semver(latestVersion);
                 updateAvailable = latestVersionSemver.isGreaterThan(versionSemver);
-                if (versionSemver.isGreaterThan(latestVersionSemver)) App.getInstance().getLogger().warn("You are running a dev version of PresenceMan");
+                if (AppInfo.development || versionSemver.isGreaterThan(latestVersionSemver)) App.getInstance().getLogger().warn("You are running a dev version of PresenceMan");
             } catch (Throwable t) {
                 updateAvailable = !AppInfo.getVersion().equals(latestVersion);
             }
             if (updateAvailable) {
                 App.getInstance().getLogger().warn("You are running an outdated version of " + AppInfo.name + "! Latest version: " + latestVersion);
-                JsonArray assets = object.getAsJsonArray("assets");
-                boolean found = false;
-                for (JsonElement asset : assets) {
-                    JsonObject assetObject = asset.getAsJsonObject();
-                    if (isPresenceManJar(object, assetObject)) {
-                        found = true;
-                        SwingUtilities.invokeLater(() -> this.showUpdateQuestion(assetObject.get("name").getAsString(), assetObject.get("browser_download_url").getAsString(), latestVersion));
-                        break;
-                    }
-                }
-                if (!found) SwingUtilities.invokeLater(() -> this.showUpdateWarning(latestVersion));
+                String latest_url = "https://github.com/Presence-Man/releases/releases/download/latest/Presence-Man-App" + (AppInfo.development ? "-dev" : "") + ".jar";
+                SwingUtilities.invokeLater(() -> this.showUpdateQuestion(latest_url, latestVersion));
             }
         } catch (Throwable ignored) {
         }
     }
 
-    private void showUpdateWarning(final String latestVersion) {
-        JOptionPane.showMessageDialog(
-                App.ui,
-                "You are running an outdated version of " + AppInfo.name + "!\nCurrent version: " + AppInfo.getVersion() + "\nLatest version: " + latestVersion, AppInfo.name,
-                JOptionPane.WARNING_MESSAGE
-        );
-    }
-
-    private void showUpdateQuestion(final String name, final String downloadUrl, final String latestVersion) {
+    private void showUpdateQuestion(final String downloadUrl, final String latestVersion) {
         int chosen = JOptionPane.showConfirmDialog(
                 App.ui,
                 "You are running an outdated version of PresenceMan!\nCurrent version: " + AppInfo.getVersion() + "\nLatest version: " + latestVersion + "\n\nDo you want to update?",
-                AppInfo.name,
+                AppInfo.name + " update",
                 JOptionPane.YES_NO_OPTION,
                 JOptionPane.QUESTION_MESSAGE
         );
         if (chosen == JOptionPane.YES_OPTION) {
-            File f = new File(name);
-            new DownloadPopup(App.ui, downloadUrl, f, () -> {
-                SwingUtilities.invokeLater(() -> {
-                    JOptionPane.showMessageDialog(App.ui, "Downloaded the latest version of PresenceMan!\nPress OK to restart.", "PresenceMan", JOptionPane.INFORMATION_MESSAGE);
+            File f = new File(AppInfo.name + "-App" + (AppInfo.development ? "-dev" : "") + " - v" + latestVersion + ".jar");
+            new DownloadPopup(
+                App.ui,
+                downloadUrl,
+                f,
+                () -> SwingUtilities.invokeLater(() -> {
+                    JOptionPane.showMessageDialog(App.ui, "Downloaded the latest version of " + AppInfo.name + "!\nPress OK to restart.", AppInfo.name, JOptionPane.INFORMATION_MESSAGE);
                     try {
                         Runtime.getRuntime().exec(new String[]{System.getProperty("java.home") + "/bin/java", "-jar", f.getAbsolutePath()});
                         System.exit(0);
                     } catch (IOException e) {
-                        App.getInstance().getLogger().error("Could not start the new PresenceMan jar", e);
+                        App.getInstance().getLogger().error("Could not start the new jar file", e);
                         App.ui.showException(e);
                     }
-                });
-            }, t -> {
-                if (t != null) {
-                    App.getInstance().getLogger().error("Could not download the latest version of PresenceMan", t);
-                    App.ui.showException(t);
+                }),
+                t -> {
+                    if (t != null) {
+                        App.getInstance().getLogger().error("Could not download the latest version of " + AppInfo.name, t);
+                        App.ui.showException(t);
+                    }
                 }
-            });
+            );
         }
     }
-
-    private boolean isPresenceManJar(final JsonObject root, final JsonObject assetObject) {
-        return assetObject.get("name").getAsString().equals(root.get("name").getAsString() + ".jar");
-    }
-
 }
