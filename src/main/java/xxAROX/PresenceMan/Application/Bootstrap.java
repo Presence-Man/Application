bfb94c8feee4f441d9c4fbb62c3a411b39ebe4b3
Jan Sohn <arox.email.0@gmail.com>
2024-01-17 10:34:28 +0000

    test

--- src/main/java/xxAROX/PresenceMan/Application/Bootstrap.java
+++ src/main/java/xxAROX/PresenceMan/Application/Bootstrap.java
@@ -6,10 +6,35 @@ import org.apache.logging.log4j.LogManager;
 public class Bootstrap {
     @SneakyThrows
     public static void main(String[] args) {
+        if (!isJVMOk()) {
+            SwingUtilities.invokeLater(() -> {
+                JOptionPane.showMessageDialog(null, 
+                    new JLabel("<html><b>Error<b>: Unsupported JVM: \""
+                        + System.getProperty("java.vm.name", "Undef") + "\" "
+                        + System.getProperty("java.vm.version", "Undef")
+                        + "<br>The JVM version required to run is 17<br>"
+                ), "Error", JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+            });
+            return;
+        }
         new App();
     }
     protected static void shutdownHook() {
         LogManager.shutdown();
         Runtime.getRuntime().halt(0); // force exit
+    }
+    public static boolean isJVMOk(){
+        boolean result = false;
+        String s = System.getProperty("java.version", "undef");
+        if (!s.equals("undef")) {
+            String parts[] = s.split("[^0-9]+");
+            if (parts.length >= 2) {
+                if (parts[0].equals("1") && parts[1].compareTo("6") >= 0) {
+                    result = true;
+                }
+            }
+        }
+        return result;
     }
 }
